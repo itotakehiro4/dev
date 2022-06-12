908807fc47a2f75fb077f8b7d9c0a1b979dfac1c
t_ito <t_ito@rrcs-172-254-99-29.nyc.biz.rr.com>
2022-06-12 11:57:28 +0900

    IDENTITY付与漏れ

--- mrs/src/main/java/mrs/domain/model/Reservation.java
+++ mrs/src/main/java/mrs/domain/model/Reservation.java
@@ -4,6 +4,9 @@ import java.io.Serializable;
 import java.time.LocalTime;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinColumns;
 import javax.persistence.ManyToOne;
@@ -16,6 +19,8 @@ public class Reservation implements Serializable {
 
 	private static final long serialVersionUID = -6557775226647678958L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer reservationId;
 
 	private LocalTime startTime;
