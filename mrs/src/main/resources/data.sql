e1b9ae77b107c74bc587c8d9b022fab86cf75254
t_ito <t_ito@rrcs-172-254-99-29.nyc.biz.rr.com>
2022-06-12 13:31:48 +0900

    SQL修正

--- mrs/src/main/resources/data.sql
+++ mrs/src/main/resources/data.sql
@@ -20,6 +20,6 @@ INSERT INTO reservable_room (reserved_date, room_id) VALUES (CURRENT_DATE + 1, 7
 -- ダミーユーザ
 INSERT INTO usr (user_id, first_name, last_name, password, role_name) VALUES ('taro-yamada', '太郎', '山田', 'bb421fa35db885ce507b0ef5c3f23cb09c62eb378fae3641c165bdf4c0272949','USER');
 -- 認証用ユーザ
-INSERT INTO usr (user_id, first_name, last_name, password, role_name) VALUES ('aaaa', 'Aaa', 'Aaa', 'USER', 'bb421fa35db885ce507b0ef5c3f23cb09c62eb378fae3641c165bdf4c0272949','USER');
-INSERT INTO usr (user_id, first_name, last_name, password, role_name) VALUES ('bbbb', 'Bbb', 'Bbb', 'USER', 'bb421fa35db885ce507b0ef5c3f23cb09c62eb378fae3641c165bdf4c0272949','USER');
-INSERT INTO usr (user_id, first_name, last_name, password, role_name) VALUES ('cccc', 'Ccc', 'Ccc', 'USER', 'bb421fa35db885ce507b0ef5c3f23cb09c62eb378fae3641c165bdf4c0272949','ADMIN');
+INSERT INTO usr (user_id, first_name, last_name, password, role_name) VALUES ('aaaa', 'Aaa', 'Aaa', 'bb421fa35db885ce507b0ef5c3f23cb09c62eb378fae3641c165bdf4c0272949','USER');
+INSERT INTO usr (user_id, first_name, last_name, password, role_name) VALUES ('bbbb', 'Bbb', 'Bbb', 'bb421fa35db885ce507b0ef5c3f23cb09c62eb378fae3641c165bdf4c0272949','USER');
+INSERT INTO usr (user_id, first_name, last_name, password, role_name) VALUES ('cccc', 'Ccc', 'Ccc', 'bb421fa35db885ce507b0ef5c3f23cb09c62eb378fae3641c165bdf4c0272949','ADMIN');
