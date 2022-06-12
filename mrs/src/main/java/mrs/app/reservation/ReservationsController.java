371eb0e00144f9798d90a7f661dacaf43c0bc4bd
t_ito <t_ito@rrcs-172-254-99-29.nyc.biz.rr.com>
2022-06-12 17:16:57 +0900

    新規アノテーションクラス作成

--- mrs/src/main/java/mrs/app/reservation/ReservationsController.java
+++ mrs/src/main/java/mrs/app/reservation/ReservationsController.java
@@ -17,12 +17,15 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RequestParam;
+import mrs.domain.model.ReservableRoom;
 import mrs.domain.model.ReservableRoomId;
 import mrs.domain.model.Reservation;
 import mrs.domain.model.Role;
 import mrs.domain.model.User;
+import mrs.domain.service.reservation.AlreadyReservedException;
 import mrs.domain.service.reservation.ReservationService;
+import mrs.domain.service.reservation.UnavailableReservationException;
 import mrs.domain.service.room.RoomService;
 
 @Controller
@@ -56,9 +59,7 @@ public class ReservationsController {
   public String reserveForm(
       @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @PathVariable("date") LocalDate date,
       @PathVariable("roomId") Integer roomId, Model model) {
-    ReservableRoomId reservableRoomId = new ReservableRoomId();
-    reservableRoomId.setRoomId(roomId);
-    reservableRoomId.setReservedDate(date);
+    ReservableRoomId reservableRoomId = new ReservableRoomId(roomId, date);
 
     List<Reservation> reservations = reservationService.findReservations(reservableRoomId);
 
@@ -73,13 +74,61 @@ public class ReservationsController {
 
   }
 
+  /**
+   * 予約
+   * @param formData
+   * @param bindingResult
+   * @param date
+   * @param roomId
+   * @param model
+   * @return
+   */
   @PostMapping
   public String reserve(@Validated ReservationForm formData, BindingResult bindingResult,
       @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @PathVariable("date") LocalDate date,
       @PathVariable("roomId") Integer roomId, Model model) {
+    
+    if (bindingResult.hasErrors()) {
+      return reserveForm(date, roomId, model);
+    }
+    
+    ReservableRoom reservableRoom = new ReservableRoom(new ReservableRoomId(roomId, date));
+    Reservation reservation = new Reservation();
+    reservation.setStartTime(formData.getStartTime());
+    reservation.setEndTime(formData.getEndTime());
+    reservation.setReservableRoom(reservableRoom);
+    reservation.setUser(dummyUser());
+    
+    try {
+      reservationService.reserve(reservation);
+    } catch (UnavailableReservationException | AlreadyReservedException e) {
+      model.addAttribute("error", e.getMessage());
+      return reserveForm(date, roomId, model);
+    }
     return "redirect:/reservations/{date}/{roomId}";
   }
 
+  /**
+   * キャンセル
+   * @param reservationId
+   * @param roomId
+   * @param date
+   * @param model
+   * @return
+   */
+  @PostMapping(params = "cancel")
+  public String cancel(@RequestParam("reservationId") Integer reservationId, @PathVariable("roomId") Integer roomId, @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @PathVariable("date") LocalDate date, Model model) {
+    
+    User user = dummyUser();
+    try {
+      reservationService.cancel(reservationId, user);
+    } catch (IllegalStateException e) {
+      model.addAttribute("error", e.getMessage());
+      return reserveForm(date, roomId, model);
+    }
+    return "redirect:/reservations/{date}/{roomId}";
+  }
+  
   /**
    * ダミーユーザ
    * 
--- mrs/src/main/java/mrs/domain/model/ReservableRoomId.java
+++ mrs/src/main/java/mrs/domain/model/ReservableRoomId.java
@@ -4,11 +4,14 @@ import java.io.Serializable;
 import java.time.LocalDate;
 
 import javax.persistence.Embeddable;
-
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Embeddable
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ReservableRoomId implements Serializable {
 
   private static final long serialVersionUID = 1992666431015096742L;
